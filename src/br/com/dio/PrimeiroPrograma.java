c66ac3a70300360dd341f64131728ad16838c0ec
GustaKowai <gusta.kowai@gmail.com>
2022-08-04 21:17:24 -0300

    segundo commit! Criando a classe livro.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
@@ -9,8 +11,45 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
-        int a = 5;
+        Livro livro1 = new Livro("O Dracula",512);
+
+        System.out.println(livro1);
+        /*int a = 5;
         int b = 3;
-        System.out.println("Hello World!" + (a+b));
+        System.out.println("Hello World!" + (a+b));*/
+    }
+}
+class Livro{
+    private String nome;
+    private Integer numPages;
+
+    public Livro(String nome, Integer numPages) {
+        this.nome = nome;
+        this.numPages = numPages;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPages=" + numPages +
+                '}';
     }
 }
